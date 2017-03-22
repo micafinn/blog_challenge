768d2bc15a0317d3a28461fc6804649772ba825d
mlf <micafinn@gmail.com>
2017-03-22 14:10:58 +0200

    Fix :id to :post_id in comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
 
     redirect_to post_path(@post)
